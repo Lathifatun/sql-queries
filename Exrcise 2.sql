c54eb4cf81170a3d9cbeb634582d3daa540f7850
Lathifatun <lathifatun.assyifa@bsi.co.id>
2025-07-15 11:07:47 +0700

    Update Exrcise 2.sql

--- Exrcise 2.sql
+++ Exrcise 2.sql
@@ -7,8 +7,16 @@ select * from HumanResources.Employee
 where HireDate > '2012-01-01';
 
 -- nomor 3
-select top 10 * from Production.Product order by ListPrice desc;
+select top 10 Name, ListPrice from Production.Product order by ListPrice desc;
 
 -- nomor 4
 select Name from Production.Product 
 where name like 'B%';
+
+-- pegawai paling muda dan paling tua
+select top 1 * from HumanResources.Employee
+order by BirthDate desc ;
+
+select top 1 * from HumanResources.Employee
+order by BirthDate asc ;
+
